ac109afedf3d1cb7bd0c8cfd6bfe21b471bab76f
essenius <essenius@users.noreply.github.com>
2020-08-10 15:47:40 +0200

    Fixed missing date changes

--- Selenium/SeleniumFixture/ApplicationInfo.cs
+++ Selenium/SeleniumFixture/ApplicationInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2015-2019 Rik Essenius
+﻿// Copyright 2015-2020 Rik Essenius
 //
 //   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file 
 //   except in compliance with the License. You may obtain a copy of the License at
@@ -18,7 +18,7 @@ namespace SeleniumFixture
     {
         public const string ApplicationName = "SeleniumFixture";
         public const string Author = "Rik Essenius";
-        public const string Copyright = "Copyright © Rik Essenius 2015-2019";
+        public const string Copyright = "Copyright © Rik Essenius 2015-2020";
         public const string Description = "A FitNesse fixture to drive Selenium WebDriver";
 
         public const string Version = "2.5.2";
