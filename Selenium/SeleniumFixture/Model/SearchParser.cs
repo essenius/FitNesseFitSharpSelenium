f040904aa645ade28e0c13ec7913290e90bbac7b
Ruchika Chadha <R.Chadha@vsodir.onmicrosoft.com>
2023-07-06 06:21:39 +0000

    Reverted css config and handled in ptm-testing repository.

--- Selenium/SeleniumFixture/Model/SearchParser.cs
+++ Selenium/SeleniumFixture/Model/SearchParser.cs
@@ -82,12 +82,10 @@ namespace SeleniumFixture.Model
             }
         }
 
-        //public static string DefaultMethod { get; set; } = "trial";
+        public static string DefaultMethod { get; set; } = "trial";
 
-        //public static string Delimiter { get; set; } = ":";
+        public static string Delimiter { get; set; } = ":";
 
-        public static string DefaultMethod { get; set; } = "CSSSELECTOR";
-        public static string Delimiter { get; set; } = "::";
         public string Locator { get; }
         public string Method { get; }
     }
